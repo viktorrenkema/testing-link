84a867a11a09825236ef00e873143ad06aaf0215
viktorrenkema <vrenkema@gmail.com>
2022-12-06 15:00:51 +0100

    Reproduce jest error

---
+++ __test__/index.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /welcome to next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+    expect(screen.getByText("Hello there")).toBeInTheDocument();
+    expect(screen.getByText("Hello there")).toHaveAttribute("href", "/bar");
+  });
+});
